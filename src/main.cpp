4434e7cd9f3807c13c7de6b4853dd7e78c8bce7a
DeadUser123 <kevinxia2015@gmail.com>
2025-01-30 09:56:26 -0500

    Skills and auton paths set up with help of https://path.jerryio.com/

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/asset.hpp"
 #include "pros/misc.h"
+
+std::string auton = "skills"; // skills, red left, red right, blue left, blue right
+// path loading
+ASSET(blueLeft1_txt);
+ASSET(blueLeft2_txt);
+ASSET(blueLeft3_txt);
+ASSET(blueRight1_txt);
+ASSET(blueRight2_txt);
+ASSET(blueRight3_txt);
+ASSET(redRight1_txt);
+ASSET(redRight2_txt);
+ASSET(redRight3_txt);
+ASSET(redLeft1_txt);
+ASSET(redLeft2_txt);
+ASSET(redLeft3_txt);
+ASSET(Skills1_txt);
+ASSET(Skills2_txt);
+ASSET(Skills3_txt);
+ASSET(Skills4_txt);
+ASSET(Skills5_txt);
+ASSET(Skills6_txt);
+ASSET(Skills7_txt);
+ASSET(Skills8_txt);
+ASSET(Skills9_txt);
+ASSET(Skills10_txt);
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
@@ -47,6 +73,16 @@ lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sens
 void toggle_clamp() {
 	clamp.set_value(!clamp_state);
 	clamp_state = !clamp_state;
+}
+
+void run_intake(bool out, bool in) {
+    if (out) {
+		intake.move(127);
+	} else if (in) {
+		intake.move(-127);
+	} else {
+		intake.move(0);
+	}
 }
 
 /**
@@ -107,7 +143,73 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+    if (auton == "skills") { // upload files onto path.jerryio.com for visualization
+        chassis.setPose(0, 0, 0);
+        chassis.follow(Skills1_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(Skills2_txt, 15, 15000, false);
+        toggle_clamp();
+        run_intake(false, false);
+        chassis.follow(Skills3_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(Skills4_txt, 15, 15000, false);
+        toggle_clamp();
+        run_intake(false, false);
+        chassis.follow(Skills5_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(Skills6_txt, 15, 15000, false);
+        toggle_clamp();
+        run_intake(false, false);
+        chassis.follow(Skills7_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(Skills8_txt, 15, 15000, false);
+        toggle_clamp();
+        run_intake(false, false);
+        chassis.follow(Skills9_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(Skills10_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, false);
+    } else if (auton == "red left") {
+        chassis.setPose(-58.467, 23.615, 86.917);
+        chassis.follow(redLeft1_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(redLeft2_txt, 15, 5000, false);
+        run_intake(false, false);
+        chassis.follow(redLeft3_txt, 15, 5000);
+    } else if (auton == "red right") {
+        chassis.setPose(-63.515, -25.216, 89.147);
+        chassis.follow(redRight1_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(redRight2_txt, 15, 5000, false);
+        run_intake(false, false);
+        chassis.follow(redRight3_txt, 15, 5000);
+    } else if (auton == "blue left") {
+        chassis.setPose(31.152, -28.28, 95.058);
+        chassis.follow(blueLeft1_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(blueLeft2_txt, 15, 5000, false);
+        run_intake(false, false);
+        chassis.follow(blueLeft3_txt, 15, 5000);
+    } else if (auton == "blue right") {
+        chassis.setPose(58.467, 23.615, 86.915);
+        chassis.follow(blueRight1_txt, 15, 5000);
+        toggle_clamp();
+        run_intake(false, true);
+        chassis.follow(blueRight2_txt, 15, 5000, false);
+        run_intake(false, false);
+        chassis.follow(blueRight3_txt, 15, 5000);
+    }
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -136,13 +238,7 @@ void opcontrol() {
 			toggle_clamp();
 		}
 
-		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-			intake.move(127);
-		} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-			intake.move(-127);
-		} else {
-			intake.move(0);
-		}
+		run_intake(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2), controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
 
 		// climb.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
 
